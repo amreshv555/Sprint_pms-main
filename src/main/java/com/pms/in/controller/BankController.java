22f42f800f87463e2943abcf61f9ac68d157e918
Vishwakarma <amresh.vishwakarma@capgemini.com>
2021-12-18 09:14:22 +0530

    c
    c

--- src/main/java/com/pms/in/controller/BankController.java
+++ src/main/java/com/pms/in/controller/BankController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.pms.in.service.AdminService;
 import com.pms.in.service.BankService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class BankController {
 
 	private static final Logger LOG = LoggerFactory.getLogger(AbstractUserController.class);
